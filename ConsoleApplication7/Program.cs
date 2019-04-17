6513d62b04461d3655fefd457af2215bf6c0e5d5
user <user@ECHTP-51-16>
2019-04-17 20:40:21 +0300

    Add project files.

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Massive();
+            Console.ReadLine();
+            
+        }
+        public static int[,] Massive()
+        {
+
+            int[,] mass = new int[3, 3];
+            printMassive(mass);
+            MassiveZna4(mass);
+            Console.WriteLine();
+            printMassive(mass);
+            return mass;
+        }
+        static Random rnd = new Random();
+        public static void printMassive(int[,] mass)
+        {
+           for (int i=0;i<3;i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                    Console.Write($"{mass[i, j]} ");
+                }
+                Console.WriteLine();
+                
+            } 
+              
+        }
+        public static void MassiveZna4(int[,] mass)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    mass[i, j] = rnd.Next(100);
+                    
+                }
+                
+
+            }
+
+        }
+    }
+}
